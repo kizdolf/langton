5f13667bd539bfbb4e3bba766e08e14b878878a8
jburet <j.buret@mektoube.fr>
2015-12-16 12:59:52 +0100

    v2 in da place. much much faster

--- README.md
+++ README.md
@@ -1,24 +1,17 @@
 # langton
-> langton ant. 
+> langton ant.
 
-dead simple, dirty code, but working.
+V2: Much much faster, change stackLoop to slow down / speedUp and to not crash your browser.
 -------
 run npm install
 
-then node index.js 
+then node index.js
 
 then open localhost:9091 and press the buttons.
 
 ------
 
-You can change the speed, the size of 'pixels', the pct of white in langton.js:
-default: 
-```
-time = 0, //time between two loops
-drawSize = 2, //2pixels to draw a square
-loop = 0, //How many loops until now? 0 is normal, don't touch it.
-pctWhite = 60, //percentage of white in the primary map.
-```
+You can change the speed, the size of 'pixels', the pct of white  and more. But changing white percent (100% by default) or the map size will fore the re-creaction of the map.
 
 ----
-the ant cannot go outside the map. Could be nice to allow it to do so, like to create more map if the ant need to.
+the ant cannot go outside the map. So if she does I just put her back in the center.
--- langtonV2.js
+++ langtonV2.js
@@ -1,31 +1,61 @@
-var height =  200,
-    width = 200,
+var height = 800,
+    width = 1500,
     black = 'black',
     white = 'white',
     antColor = 'red',
     map = {},
-    pos = {x: null, y: null};
+    up = 1, left = 2, bot = 3, right = 4,
+    loop = 0,
+    pos = {x: null, y: null},
+    direction = up,
+    time = null,
+    stackLoop = 3000;
 
-var pctWhite = 70,
-    squareSize = 4;
+var pctWhite = 100,
+    squareSize = 1;
+
+var toRedraw, color;
 
 var c = document.getElementById('mainFrame'),
     fr = $('#mainFrame'),
     ctx = c.getContext('2d');
 
 $('#fulfill').click(function(){fulfill();});
-// $('#timer').change(function(){time=$(this).val();});
-$('#placeAnt').click(function(){placeAnt();});
-
-$('#start').click(function(){startTheGame();});
-
-$('#zoom').change(function(){
-    var val = $(this).val();
-    console.log(val);
-    squareSize=val;
+$('#timer').change(function(){
+    time=$(this).val();
+    displayVals();
 });
-
-
+// $('#placeAnt').click(function(){placeAnt();});
+$('#start').click(function(){startTheGame();});
+$('#zoom').change(function(){
+    fr.attr('height', (height * squareSize));
+    fr.attr('width', (width * squareSize));
+    squareSize = $(this).val();
+    displayVals();
+});
+$('#height').change(function(){
+    height = $(this).val();
+    displayVals();
+    fulfill();
+});
+$('#width').change(function(){
+    width = $(this).val();
+    displayVals();
+    fulfill();
+});
+$('#pctWhite').change(function(){
+    pctWhite = $(this).val();
+    displayVals();
+    fulfill();
+});
+var l = $('#loop');
+var displayVals = function(){
+    l.html(loop);
+    $('#curHeight').html(height);
+    $('#curWidth').html(width);
+    $('#curWhite').html(pctWhite);
+    $('#curTime').html(time);
+};
 
 var fulfill = function(){
     for (var x = 0; x < width; x++) {
@@ -39,13 +69,16 @@ var fulfill = function(){
             }
         }
     }
-    buildCanvas();
-};
-
-var buildCanvas = function(){
-
     fr.attr('height', (height * squareSize));
     fr.attr('width', (width * squareSize));
+    pos.x = Math.ceil(width / 2);
+    pos.y = Math.ceil(height / 2);
+    direction = up;
+    buildCanvas();
+    displayVals();
+};
+
+var buildCanvas = function(cb){
     for (var x = 0; x < width; x++) {
         for (var y = 0; y < height; y++) {
             var color = map[x][y].c;
@@ -53,12 +86,64 @@ var buildCanvas = function(){
             ctx.fillRect((x*squareSize), (y*squareSize), squareSize, squareSize);
         }
     }
+    placeAnt();
+    if(cb){
+        cb();
+    }
 };
 
 var placeAnt = function(){
-    pos.x = Math.ceil(width / 2);
-    pos.y = Math.ceil(height / 2);
-    console.log(pos);
     ctx.fillStyle = antColor;
     ctx.fillRect((pos.x*squareSize), (pos.y*squareSize), squareSize, squareSize);
+};
+
+var reDraw = function(r){
+    ctx.fillStyle = antColor;
+    ctx.fillRect((pos.x*squareSize), (pos.y*squareSize), squareSize, squareSize);
+    ctx.fillStyle = r.color;
+    ctx.fillRect((r.x*squareSize), (r.y*squareSize), squareSize, squareSize);
+};
+
+var buildMore = function(){
+    if(pos.x <= 0) pos.x = 0;
+    if(pos.y <= 0) pos.y = 0;
+    if(pos.x >= height) pos.x = height - 1;
+    if(pos.y >= width) pos.y = width - 1;
+};
+
+var startTheGame = function(){
+    color = map[pos.x][pos.y].c;
+    if(color == white) direction = ((direction + 1) > 4) ? 1 : (direction + 1);
+    else direction = ((direction - 1) >= 1) ? (direction - 1) : 4;
+    map[pos.x][pos.y].c = (color == white) ? black : white;
+    toRedraw = {x: pos.x, y: pos.y, color: (color == white) ? black : white};
+    if(direction == up) pos.y = pos.y - 1;
+    else if (direction == left) pos.x = pos.x - 1;
+    else if (direction == bot) pos.y = pos.y + 1;
+    else pos.x = pos.x + 1;
+    reDraw(toRedraw);
+    ++loop;
+    again();
+    return;
+};
+
+var again = function(){
+    if(!map[pos.x] || !map[pos.x][pos.y]){
+        console.log('reposition');
+        pos.x = Math.ceil(width / 2);
+        pos.y = Math.ceil(height / 2);
+        setTimeout(function(){
+            l.html(loop);
+            startTheGame();
+        },0);
+    }
+    if((loop % stackLoop) === 0){
+        //clear queue to not reach maximum call stack size.
+        setTimeout(function(){
+            l.html(loop);
+            startTheGame();
+        },0);
+    }else{
+        startTheGame();
+    }
 };
